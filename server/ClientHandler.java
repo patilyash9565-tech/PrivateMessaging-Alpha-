6d1f7ababe4073928b5aa0286bf418e99d7da3a5
Yash Patil <yashpatil@Mac.lan>
2026-08-09 18:59:06 +0530

    Phase 7 complete - username identity and validation

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -11,13 +11,14 @@ private Socket socket;
 private BufferedReader reader;
 private PrintWriter writer;
 private int clientId;
+private String username;
 private static int nextClientId = 1;
 private static List<ClientHandler> clients = new ArrayList<>();
 
 public ClientHandler(Socket socket) {
     this.socket = socket;
     this.clientId = nextClientId++;
-    clients.add(this);
+    
 
     try {
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -42,6 +43,17 @@ public static ClientHandler findClient(int clientId) {
     return null;
 }
 
+public static ClientHandler findClientByUsername(String username) {
+
+    for (ClientHandler client : clients) {
+        if (client.username.equalsIgnoreCase(username)) {
+            return client;
+        }
+    }
+
+    return null;
+}
+
 @Override
 public void run() {
     System.out.print("Handling client: " + clientId + " : "
@@ -50,8 +62,29 @@ public void run() {
     writer.println("Welcome to the chat server! You are connected as: "
             + socket.getInetAddress().getHostAddress());
 
+     
+
     try {
         String message;
+
+        writer.println("Enter your username:");
+
+       while(true){
+            username = reader.readLine();   
+            if (username==null || username.trim().isEmpty()){
+                writer.println("User name cannot be empty. Try again: ");
+                continue;
+            }
+
+            if (findClientByUsername(username)!=null){
+                writer.println("User name is already taken. Try another: ");
+                continue;
+            }
+            username = username.trim();
+            break;
+        }  
+
+        clients.add(this);
 
         while ((message = reader.readLine()) != null) {
             System.out.println("RAW MESSAGE RECEIVED >>> " + message);
@@ -60,12 +93,12 @@ public void run() {
                 String[] parts = message.split(" ", 3);
 
                 if (parts.length < 3) {
-                    writer.println("Usage: /msg <clientId> <message>");
+                    writer.println("Usage: /msg <userName> <message>");
                     continue;
                 }
 
-                try {
-                    int targetId = Integer.parseInt(parts[1]);
+                
+                    String targetUsername = parts[1];
                     String privateMessage = parts[2];
 
                     if (privateMessage.trim().isEmpty()) {
@@ -73,22 +106,20 @@ public void run() {
                         continue;
                     }
 
-                    ClientHandler targetClient = findClient(targetId);
+                    ClientHandler targetClient = findClientByUsername(targetUsername);
 
                     if (targetClient != null) {
                         targetClient.writer.println(
-                                "Private message from Client "
-                                + clientId + ": " + privateMessage);
+                                "Private message from "
+                                + username + ": " + privateMessage);
                     } else {
-                        writer.println("Client " + targetId + " not found.");
+                        writer.println("User " + targetUsername + " not found.");
                     }
 
-                } catch (NumberFormatException e) {
-                    writer.println("Invalid client Id. Please use a number.");
-                }
+                
 
             } else {
-                broadcast("Client " + clientId + ": " + message);
+                broadcast(username + ": " + message);
             }
         }
 
